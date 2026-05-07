0a617ec89008e5ed05a1fbd7c9394dc0568ec33f
Arushi-beehive <arushi@beehiveinfotech.com>
2026-05-07 11:24:28 +0530

    fn chnage

--- optimus-models/lib/controllers/fnupserttransactionmisc.js
+++ optimus-models/lib/controllers/fnupserttransactionmisc.js
@@ -15,7 +15,7 @@ module.exports.fnupserttransactionmisc = (req, res, next)=>{
     console.log('The bosy is',clientschema)
 
     return new Promise((resolve,reject)=>{
-        db.query('select' + clientschema + ".fn_delete_transaction_misc($1::int,$2::text,$3::text,$4::int,$5::text)",
+        db.query('select' + clientschema + ".fn_upsert_transaction_misc($1::int,$2::text,$3::text,$4::int,$5::text)",
             [p_transaction_id, p_transaction_date, p_head, p_amount, p_username],
             (err, result)=>{
                 if(err){
@@ -26,7 +26,7 @@ module.exports.fnupserttransactionmisc = (req, res, next)=>{
                 }else{
                     response["success"]=true;
                     response["message"]="data fetch";
-                    response["data"]= result.rows[0].fn_delete_transaction_misc;
+                    response["data"]= result.rows[0].fn_upsert_transaction_misc;
                     return resolve(response);
                 }
             },
